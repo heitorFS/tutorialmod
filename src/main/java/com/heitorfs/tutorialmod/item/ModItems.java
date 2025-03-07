5db0f9c63d4cc4588d6d51585049d81cd6891160
heitorFS <heitor.freitassilva@oulook.com>
2025-03-07 15:34:34 -0300

    strogonoff added fully functionally

--- src/main/java/com/heitorfs/tutorialmod/item/ModItems.java
+++ src/main/java/com/heitorfs/tutorialmod/item/ModItems.java
@@ -2,6 +2,7 @@ package com.heitorfs.tutorialmod.item;
 
 import com.heitorfs.tutorialmod.TutorialMod;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -15,6 +16,10 @@ import java.util.function.Function;
 public class ModItems {
     private static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", Item::new, new Item.Settings());
     private static final Item PINK_GARNET = registerItem("pink_garnet", Item::new, new Item.Settings());
+    private static final Item RICE = registerItem("rice", Item::new, new Item.Settings().food(new FoodComponent.Builder().nutrition(4).saturationModifier(1).build()));
+    private static final Item TOMATO = registerItem("tomato", Item::new, new Item.Settings().food(new FoodComponent.Builder().nutrition(2).saturationModifier(2.5f).build()));
+    private static final Item MUSTARD = registerItem("mustard", Item::new, new Item.Settings().food(new FoodComponent.Builder().nutrition(1).build()));
+    private static final Item STROGONOFF = registerItem("strogonoff", Item::new, new Item.Settings().food(new FoodComponent.Builder().nutrition(12).saturationModifier(14).build()));
 
     private static Item registerItem(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
         RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TutorialMod.MOD_ID, name));
@@ -29,6 +34,13 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PINK_GARNET);
             entries.add(RAW_PINK_GARNET);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+            entries.add(RICE);
+            entries.add(TOMATO);
+            entries.add(MUSTARD);
+            entries.add(STROGONOFF);
         });
     }
 }
